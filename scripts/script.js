72de8ca0d3282506d3fa294fb22c5b90e07777b3
GreenBeansAgainstVeganism <kradrling@gmail.com>
2023-02-03 14:22:22 -0800

    Fancier link menu, curry description

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,10 @@ window.onload = () => {
         procedure: 'faq.html#what-is-a-procedure',
         procedures: 'faq.html#what-is-a-procedure',
         proc: 'faq.html#what-is-a-procedure',
+        list: 'faq.html#what-are-pairs-and-lists',
+        lists: 'faq.html#what-are-pairs-and-lists',
+        pair: 'faq.html#what-are-pairs-and-lists',
+        pairs: 'faq.html#what-are-pairs-and-lists',
         racket: 'faq.html#what-is-racket',
         lisp: 'faq.html#lisp-dialects',
         lambda: 'functionguide.html#func-lambda',
@@ -21,7 +25,9 @@ window.onload = () => {
     }
     const sections = [...document.getElementsByClassName('linkgen')];
     sections.forEach(s=>{
+        // Get the url that corresponds with this keyword
         const href = keywords[s.innerHTML.toLowerCase()];
+        // If the keyword exists, replace this tag with an anchor, otherwise remove the tag
         s.outerHTML = href ? `<a href="${href}">${s.innerHTML}</a>` : s.innerHTML;
     });
 };
